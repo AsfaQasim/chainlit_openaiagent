fc76c340284f37ee8d946dd64abf1e6e9c4010a8
Asifa Muhammad Qasim <160766420+AsfaQasim@users.noreply.github.com>
2025-06-10 16:10:53 +0500

    main

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
 from agents import Agent, agent, Runner, OpenAIChatCompletionsModel, AsyncOpenAI, RunConfig
 import os
 from dotenv import load_dotenv
+import chainlit as cl
 
 load_dotenv()
 
 gemini_api_key = os.getenv("Gemini_Api_key")
 
 external_client = AsyncOpenAI(
-    api_key = gemini_api_key,
-    base_url = "https://generativelanguage.googleapis.com/v1beta/openai/"
+    api_key=gemini_api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
 )
 
-model  = OpenAIChatCompletionsModel(
-    model = "gemini-2.0-flash",
-    openai_client = external_client
-
+model = OpenAIChatCompletionsModel(
+    model="gemini-2.0-flash",
+    openai_client=external_client
 )
 
 config = RunConfig(
-  model =  model,
-  model_provider = external_client,
-  tracing_disabled= True
+    model=model,
+    model_provider=external_client,
+    tracing_disabled=True
 )
 
 agent = Agent(
@@ -28,11 +28,22 @@ agent = Agent(
     instructions="You are a frontend expert"
 )
 
+@cl.on_chat_start
+async def handle_Start():
+    cl.user_session.set("history", [])
+    await cl.Message(content= "Hello this is asfa how can I help you!").send()
 
-result = Runner.run_sync(
-    agent,
-     input = "Hello how are you!",
-     run_config = config
-)
-
-print(result.final_output)
+@cl.on_message
+async def handle_message(message: cl.Message):
+    history = cl.user_session.get("history")
+    history.append({"role": "user", "content": message.content})
+    content = history
+    # Process the message here
+    result = await Runner.run(
+        agent,
+        input=content,
+        run_config=config
+    )
+    history.append({"role": "assistant", "content": result.final_output})
+    cl.user_session.set("history", history)
+    await cl.Message(content=result.final_output).send()
